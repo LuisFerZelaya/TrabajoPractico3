62a375f6762ccacede606086ffa7f34a50765471
Fernando <fernandozelaya9@gmail.com>
2022-06-13 16:41:30 -0300

    first commit

---
+++ src/punto8.java
@@ -0,0 +1,15 @@
+public class punto8 {
+    public static void main(String[] args) {
+        System.out.println("Con los numeros 16 y 8");
+        int num1 = 16;
+        int num2 = 8;
+        int suma = num1 + num2;
+        int resta = num1 - num2;
+        int producto = num1 * num2;
+        int division = num1 / num2;
+        System.out.println("el resultado de su suma es = " + suma);
+        System.out.println("el resultado de su resta es = " + resta);
+        System.out.println("el resultado de su multiplcacion es = " + producto);
+        System.out.println("el resultado de su division es = " + division);
+    }
+}
